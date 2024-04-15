ad0b89b220d8a3c8e2cc9746f7b53ed8917cf105
mjsri <srikanthchowdary1994@gmail.com>
2024-04-15 17:45:19 +0530

    push terraform

--- session-1/ec2/ec2.tf
+++ session-1/ec2/ec2.tf
@@ -1,36 +1,35 @@
 resource "aws_instance" "web" {
-  ami           = "ami-03265a0778a880afb" #devops-practice
+  ami           = "ami-0f3c7d07486cad139"  #devops-practice
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id] # [] means list
 
   tags = {
     Name = "HelloTerraform"
   }
 }
 
-resource "aws_security_group" "roboshop-all" { #this is terraform name, for terraform reference only
-    name        = var.sg-name # this is for AWS
-    description = var.sg-description
-    #vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "roboshop-all" { #this is terraform name for terraform reference only
+  name = var.sg-name #this is for aws name
+  description = var.sg-description
+  #vpc_id = aws_vpc.main.id
 
-    ingress {
-        description      = "Allow All ports"
-        from_port        = var.inbound-from-port # 0 means all ports
-        to_port          = 0 
-        protocol         = "tcp"
-        cidr_blocks      = var.cidr_blocks
-        #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-    }
+  ingress {
+      description = "Allow all ports"
+      from_port = var.inbound-from-port #o means all ports
+      to_port = 0
+      protocol = "tcp"
+      cidr_blocks = var.cidr_blocks
+  }
 
-    egress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-        #ipv6_cidr_blocks = ["::/0"]
-    }
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      #ipv6_cidr_blocks = ["::/0"]
+  }
 
-    tags = {
-        Name = "roboshop-all-aws"
-    }
+  tags = {
+    Name ="roboshop-all-aws"
+  }
 }
